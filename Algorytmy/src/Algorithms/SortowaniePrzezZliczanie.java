e2a9f1ad1ddb3ae1f0b76b3d89111dddb68a8788
Lukas <lukas.wojtkowiak@gmail.com>
2017-07-28 17:05:58 +0200

    aktualizacja danych

--- Algorytmy/src/Algorithms/SortowaniePrzezZliczanie.java
+++ Algorytmy/src/Algorithms/SortowaniePrzezZliczanie.java
@@ -2,25 +2,40 @@ package Algorithms;
 
 
 public class SortowaniePrzezZliczanie extends AbstractAlgorithm {
+
     @Override
     public String getName() {
-        return "sortowanie przez zliczanie";
+        return "Sortowanie przez zliczanie";
     }
 
     @Override
-    public void runAlgorithm(String[] input) {
+    public void runAlgorithm(String[] input)
+    {
+        int n = input.length - 2;
+        int max = Integer.parseInt(input[1]) + 1;
+        int[] tabSparsowanychWejscia = new int[n];
 
-//        int n = input.length -2;
-//        int max = input
-//        int [] tab = new int[n + 1];
-//
-//        for (int i = 0; i < n ; i++) {
-//            tab[i] = Integer.parseInt(input[i -3]);
-//        }
-//
-//        int [] tab2 = new int[n];
-//        for (int i = 0; i < ; i++) {
-//
-//        }
+        for (int i = 0; i < n; i++)
+        {
+            tabSparsowanychWejscia[i] = Integer.parseInt(input[i+2]);
+        }
+
+        int[] tablicaDoZliacznia = new int[max];
+        for (int i = 0; i < tabSparsowanychWejscia.length; i++ )
+        {
+            int liczba = tabSparsowanychWejscia[i];
+            tablicaDoZliacznia[liczba]++;
+        }
+
+        for(int indeksTablicy = 0; indeksTablicy < tablicaDoZliacznia.length; indeksTablicy++)
+        {
+            if(tablicaDoZliacznia[indeksTablicy] > 0)
+                for(int ileLiczb = tablicaDoZliacznia[indeksTablicy]; ileLiczb > 0; ileLiczb--)
+
+                {
+
+                    System.out.printf("%d ", indeksTablicy);
+                }
+        }
     }
 }
